3f3767f10691802eb1b2a8219f8e2c5ea1cefd69
Paulo Luan <pauloluan.inova@gmail.com>
2015-06-01 09:42:53 -0300

    removed HTML5 ui-route style. Fixes #1

--- src/app/index.js
+++ src/app/index.js
@@ -2,15 +2,13 @@
 
 angular.module('lawsApp', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngResource', 'ui.router', 'ngMaterial', 'markdown'])
     .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-        var base_path = "";
-
         $urlRouterProvider.rule(function ($injector, $location) {
             var path = $location.path();
             var hasTrailingSlash = path[path.length - 1] === '/';
 
             if (hasTrailingSlash) {
                 //if last charcter is a slash, return the same url without the slash  
-                base_path = path.substr(0, path.length - 1);
+                var base_path = path.substr(0, path.length - 1);
                 return base_path;
             }
         });
@@ -23,30 +21,30 @@ angular.module('lawsApp', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngResource',
             })
 
             .state('politicianList', {
-                url: base_path + '/politicos',
+                url: '/politicos',
                 templateUrl: 'app/politician/list/politician_list.html',
                 controller: 'PoliticianListController'
             })
 
             .state('politicianDetails', {
-                url: base_path + '/politicos/:itemId',
+                url: '/politicos/:itemId',
                 templateUrl: 'app/politician/details/politician_details.html',
                 controller: 'PoliticianDetailsController'
             })
 
             .state('lawList', {
-                url: base_path + '/leis',
+                url: '/leis',
                 templateUrl: 'app/laws/list/laws_list.html',
                 controller: 'LawListController'
             })
 
             .state('lawDetails', {
-                url: base_path + '/leis/:itemId',
+                url: '/leis/:itemId',
                 templateUrl: 'app/laws/details/law_details.html',
                 controller: 'LawDetailsController'
             });
 
-        $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
         $urlRouterProvider.otherwise('/');
     })
 ;
