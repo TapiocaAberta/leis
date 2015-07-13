d58af8e05c6e7ce1dc8ea49b05c16f2f39a4cda8
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-13 13:27:46 -0300

    Update 2015-07-13T16:27:38.680Z

--- src/app/laws/list/laws_list.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('lawsApp')
-    .controller('LawListController', function ($scope, $http) {
-        $http.get('data/laws.json').success(function (data) {
-            $scope.laws = data;
-            $scope.lawOrder = 'name';
-        });
-    });
--- src/app/main/main.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('lawsApp')
-    .controller('MainCtrl', function ($scope) {
-
-    });
