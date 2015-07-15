d7e59abebf972b9587211d338f19c4ff3ff3ab69
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-15 17:02:14 -0300

    added new style for politicians profile

--- src/app/politician/list/politician.controller.js
+++ src/app/politician/list/politician.controller.js
@@ -2,7 +2,10 @@
     'use strict';
 
     angular.module('lawsApp')
-        .controller('PoliticianCtrl', function($scope) {
-            $scope.message = 'Hello';
+        .controller('PoliticianCtrl', function($scope, $http) {
+            $http.get('data/vereadores.json').success(function(data) {
+                $scope.politicians = data;
+                $scope.politicianOrder = 'name';
+            });
         });
 })();
