83f4675eda510e58b29b39ce0902601846586ea1
Paulo Luan <pauloluan.inova@gmail.com>
2016-11-29 16:57:19 -0200

    added first version of voting system

--- src/app/laws/details/lawDetails.controller.js
+++ src/app/laws/details/lawDetails.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('lawsApp')
-        .controller('LawDetailsCtrl', function($scope, $http, $stateParams, URI) {
+        .controller('LawDetailsCtrl', function($scope, $http, $stateParams, URI, toastr) {
 
             $scope.successVote = false;
             $scope.errorVote = false;
@@ -19,27 +19,33 @@
 
             }
 
-            $scope.vote = function(url) {
+            $scope.vote = function(rating) {
+                var url = "http://temis-server.herokuapp.com/api/laws/"+ $scope.law.code +"/vote?rating=" + rating;
 
                 $http.put(url).success(function(data) {
-
                     $scope.law = data;
                     $scope.successVote = true;
                     $scope.message = 'Voto computado!';
+
+                    toastr.success('Voto computado!', '');
 
                 }).error(function(error) {
                     console.log(error);
                     $scope.errorVote = true;
                     $scope.message = error.message;
+
+                    toastr.success('Voto computado!', '');
                 });
 
             };
 
-            $scope.hoveringOver = function(value) {
-                $scope.overStar = value;
-                $scope.percent = 100 * (value / $scope.max);
-            };
-
+            $scope.clickRating = function(rating) {
+                if(Number.isInteger(rating)) {
+                    if (0 < rating && rating < 6) {
+                        $scope.vote(rating);
+                    }
+                }
+            }
 
         });
 })();
