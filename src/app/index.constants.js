9e131d49e6bec17c0bceb2e69c259d8f1d9250ba
Paulo Luan <pauloluan.inova@gmail.com>
2016-11-29 15:05:23 -0200

    removed malarkey unused dep

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,10 +1,9 @@
-/* global malarkey:false, toastr:false, moment:false */
+/* global toastr:false, moment:false */
 (function() {
     'use strict';
 
     angular
         .module('lawsApp')
-        .constant('malarkey', malarkey)
         .constant('toastr', toastr)
         .constant('moment', moment)
         //.constant('URI', 'http://localhost:8080/api/');
