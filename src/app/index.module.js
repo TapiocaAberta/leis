2f9843478b5ec8b93815d0e6e8b784ad156ac012
nicolastarzia <github@nicolastarzia.com>
2017-06-19 16:00:49 -0300

    :art: Adicionar spin em requisições AJAX

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,6 +5,23 @@
     angular
         .module('lawsApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize',
             'ngResource', 'ui.router', 'ui.bootstrap', 'mgcrea.ngStrap', 'angular-input-stars'
-        ]);
+        ]).directive('loading', ['$http', function($http){
+            return {
+                restrict: 'A',
+                link: function (scope, elm, attrs){
+                    scope.isLoading = function(){
+                        return $http.pendingRequests.length > 0;
+                    };
+
+                    scope.$watch(scope.isLoading, function(v){
+                        if(v){
+                            elm.removeAttr('style');
+                        }else{
+                            elm.attr('style','display:none');
+                        }
+                    });
+                }
+            }
+        }]);
 
 })();
