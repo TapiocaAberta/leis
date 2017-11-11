0b2ddb64365a97e1922b8502c0c2c2156cb778bb
pedro-hos <pedro-hos@outlook.com>
2017-11-11 16:04:22 -0200

    funcionadade de voto, integração com api

--- src/app/laws/details/lawDetails.controller.js
+++ src/app/laws/details/lawDetails.controller.js
@@ -4,14 +4,17 @@
     angular.module('lawsApp')
         .controller('LawDetailsCtrl', function($scope, $http, $stateParams, URI, toastr) {
 
+          $scope.messageSuccess = '';
+          $scope.messageError = '';
+
             if ($stateParams.id) {
 
                 $http.get(URI + 'leis/' + $stateParams.id).success(function(data) {
                         $scope.law = data;
-                    })
-                    .error(function(error) {
-                        console.log(error);
-                    });
+
+                }) .error(function(error) {
+                  console.log(error);
+                });
 
             }
 
@@ -21,7 +24,19 @@
                         $scope.vote(rating);
                     }
                 }
-            }
+            };
 
+            $scope.vote = function(rating) {
+
+              $http.put(URI + 'leis/' + $stateParams.id + '?rating=' + rating).success(function(data) {
+
+                 $scope.law = data;
+                 $scope.messageSuccess = 'Voto computado!';
+
+              }) .error(function(error) {
+                console.log(error);
+                $scope.messageError = error.mensagem;
+              });
+            };
         });
 })();
