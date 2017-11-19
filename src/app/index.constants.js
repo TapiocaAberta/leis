7c265d8c4c8b2ea5171f92e21b1373784f4e12e6
pedro-hos <pedro-hos@outlook.com>
2017-11-19 18:06:33 -0200

    url de prod

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -5,6 +5,6 @@
     angular
         .module('lawsApp')
         .constant('moment', moment)
-        .constant('URI', 'http://localhost:8080/api/v2/');
-        //.constant('URI', 'http://104.131.106.85:8080/api/v2/');
+        //.constant('URI', 'http://localhost:8080/api/v2/');
+        .constant('URI', 'http://104.131.106.85:8080/api/v2/');
 })();
